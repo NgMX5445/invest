0272bde805d646369e046f830b402ba0b83f9109
[200~ <laohuang@gmail.com>
2023-04-15 22:44:49 +0800

    counting min

--- public/demoTable/index.js
+++ public/demoTable/index.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
     let times = 0;
     let timeNow = new Date();
     let hourNow = timeNow.getHours();
- 
+    let minNow = timeNow.getMinutes()
     while (times < 24) {
         times = times + 1;
 
@@ -13,12 +13,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
         row.querySelector('#days').innerHTML = times + ":00";
         row.querySelector('#days').id = times;
+
         document.querySelector('#Displaydays').appendChild(row);
     }
-  
-     
-    document.getElementById(hourNow).className = "line";
 
-   
+    let x = ((hourNow - 1) / 24 * 60);
+
+    let y = x * 5000;
+
+    document.getElementById("line").style.width = `${y}px`
+
+
 
 })
