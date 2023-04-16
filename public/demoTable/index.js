589bd7bb87b14cf68b5426321377bdadb22052fe
[200~ <laohuang@gmail.com>
2023-04-16 10:46:03 +0800

    test1

--- public/demoTable/index.js
+++ public/demoTable/index.js
@@ -1,28 +1,32 @@
 
 document.addEventListener("DOMContentLoaded", () => {
-    let times = 0;
-    let timeNow = new Date();
-    let hourNow = timeNow.getHours();
-    let minNow = timeNow.getMinutes()
-    while (times < 24) {
-        times = times + 1;
+  let times = 0;
+  let timeNow = new Date();
+  let hourNow = timeNow.getHours();
+  let minNow = timeNow.getMinutes();
+  let sNow = timeNow.getSeconds();
+  while (times < 24) {
+    times = times + 1;
 
 
-        const templates = document.querySelector('#tableDay');
-        const row = templates.content.cloneNode(true);
+    const templates = document.querySelector('#tableDay');
+    const row = templates.content.cloneNode(true);
 
-        row.querySelector('#days').innerHTML = times + ":00";
-        row.querySelector('#days').id = times;
+    row.querySelector('#days').innerHTML = times + ":00";
+    row.querySelector('#days').id = times;
 
-        document.querySelector('#Displaydays').appendChild(row);
-    }
+    document.querySelector('#Displaydays').appendChild(row);
+  }
 
-    let x = ((hourNow - 1) / 24 * 60);
+  let x = (((((hourNow - 1) * 60) + minNow) * 60) + sNow) / (1440 * 60);
 
-    let y = x * 5000;
+  let y = x * 5000;
 
-    document.getElementById("line").style.width = `${y}px`
-
+  document.getElementById("line").style.width = `${100}%`
+  //AutoRefresh(500)
+  function AutoRefresh(t) {
+    setTimeout("location.reload(true);", t);
+  }
 
 
 })
